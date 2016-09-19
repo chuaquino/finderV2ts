f7df1d2d6dc6e78ba46f0edc9bf0440bf43b87ad
rklumanlan <rklumanlan@gmail.com>
2016-09-19 13:40:18 +0800

    finderV2 ts initial

---
+++ app/pages/landingpage/landingpage.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {NavParams, NavController, AlertController} from 'ionic-angular';
+
+
+@Component({
+  templateUrl: 'build/pages/landingpage/landingpage.html'
+})
+export class LandingPage {
+
+
+}
